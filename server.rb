4801dae1b95850bea355da8201c9da3b821b21fe
Peter Kong <bansuki@gmail.com>
2017-11-26 15:18:20 -0600

    more cors tinkering

--- server.rb
+++ server.rb
@@ -8,11 +8,14 @@ configure do
   enable :cross_origin
 end
 
+before do
+  response.headers['Access-Control-Allow-Origin'] = '*'
+end
+
 options "*" do
-  response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
-
-  response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
-
+  response.headers["Allow"] = "GET, POST, OPTIONS"
+  response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+  response.headers["Access-Control-Allow-Origin"] = "*"
   200
 end
 
