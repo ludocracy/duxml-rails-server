6bd9007c0fbbd5cad8762c44e88e457f1f21aaee
Peter Kong <bansuki@gmail.com>
2017-11-26 15:41:30 -0600

    again

--- server.rb
+++ server.rb
@@ -1,18 +1,16 @@
 require 'sinatra'
 require 're_duxml'
-require 'sinatra/cors'
 
 include ReDuxml
 
-
-set :allow_origin, "*" #"https://duxml.herokuapp.com https://mysterious-castle-11258.herokuapp.com"
-
 post '/resolveXML' do
+  response['Access-Control-Allow-Origin'] = '*'
   doc = Saxer.sax request.body.read
   resolve(doc, params).to_s
 end
 
 post '/evaluateStr' do
+  response['Access-Control-Allow-Origin'] = '*'
   str = request.body.read
   Evaluator.new.evaluate(str, params).to_s
 end
