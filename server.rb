786b8ce1083816c4a6d8f09f45ff292a00b0ef6c
Peter Kong <bansuki@gmail.com>
2017-11-26 15:12:37 -0600

    more cors business: responding to OPTIONS

--- server.rb
+++ server.rb
@@ -8,6 +8,14 @@ configure do
   enable :cross_origin
 end
 
+options "*" do
+  response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
+
+  response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
+
+  200
+end
+
 post '/resolveXML' do
   doc = Saxer.sax request.body.read
   resolve(doc, params).to_s
