5836420a5660945c30575d8dc14cb93d1d05bf9b
Peter Kong <bansuki@gmail.com>
2017-11-25 23:04:11 -0600

    fixed issue and cleaned up code

--- server.rb
+++ server.rb
@@ -3,15 +3,13 @@ require 're_duxml'
 
 include ReDuxml
 
-@e = Evaluator.new
-
 post '/resolveXML' do
   doc = Saxer.sax request.body.read
-  puts params
-  resolve(doc, params).to_s
+  answer = resolve(doc, params).to_s
+  puts "answer=#{answer}"
 end
 
 post '/evaluateStr' do
   str = request.body.read
-  Evaluator.new.evaluate(str, params)
+  Evaluator.new.evaluate(str, params).to_s
 end
