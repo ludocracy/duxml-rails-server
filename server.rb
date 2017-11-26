5c246678827f29c333faf06bcdad7b1a42645a2e
Peter Kong <bansuki@gmail.com>
2017-11-26 15:45:32 -0600

    again

--- server.rb
+++ server.rb
@@ -4,13 +4,13 @@ require 're_duxml'
 include ReDuxml
 
 post '/resolveXML' do
-  response['Access-Control-Allow-Origin'] = '*'
+  response.headers['Access-Control-Allow-Origin'] = '*'
   doc = Saxer.sax request.body.read
   resolve(doc, params).to_s
 end
 
 post '/evaluateStr' do
-  response['Access-Control-Allow-Origin'] = '*'
+  response.headers['Access-Control-Allow-Origin'] = '*'
   str = request.body.read
   Evaluator.new.evaluate(str, params).to_s
 end
