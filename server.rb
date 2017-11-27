d6ca86ed40229de9a7eb0f03aa2ba4a19cf746c0
Peter Kong <bansuki@gmail.com>
2017-11-26 23:18:59 -0600

    twice

--- server.rb
+++ server.rb
@@ -5,12 +5,18 @@ include ReDuxml
 
 post '/resolveXML' do
   response.headers['Access-Control-Allow-Origin'] = '*'
+  response['Access-Control-Allow-Origin'] = '*'
+
   doc = Saxer.sax request.body.read
   resolve(doc, params).to_s
 end
 
 post '/evaluateStr' do
   response.headers['Access-Control-Allow-Origin'] = '*'
+  response['Access-Control-Allow-Origin'] = '*'
+
   str = request.body.read
-  Evaluator.new.evaluate(str, params).to_s
+  result = Evaluator.new.evaluate(str, params || {})
+  puts "evaluate(#{str}, #{params}) => #{result.to_s}"
+  result.to_s
 end
