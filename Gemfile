d223585ac9f392d651c7e88441aacdf82897c71e
Peter Kong <bansuki@gmail.com>
2017-11-28 12:25:09 -0600

    fixed all but one phantom param

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
 source 'https://rubygems.org'
 ruby '~> 2.3.1'
 gem 'sinatra'
-gem 'sinatra-cors'
 gem 're_duxml', '~> 0.2.3'
--- server.rb
+++ server.rb
@@ -7,23 +7,27 @@ include ReDuxml
 origin_domains = '*'
 
 options '*' do
-    response.headers['Access-Control-Allow-Origin'] = origin_domains
-    response['Access-Control-Allow-Origin'] = origin_domains
+  response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
+  200
+end
+
+before do
+  response['Access-Control-Allow-Origin'] = origin_domains
+  response['Content-Type'] = 'application/json'
 end
 
 post '/resolveXML' do
-  response.headers['Access-Control-Allow-Origin'] = origin_domains
-  response['Access-Control-Allow-Origin'] = origin_domains
-
-  doc = Saxer.sax request.body.read
+  puts "params = #{params}"
+  xmlStr = JSON.parse(request.body.read)["xml"]
+  doc = Saxer.sax xmlStr
   resolve(doc, params).to_s
 end
 
 post '/evaluateStr' do
-  response.headers['Access-Control-Allow-Origin'] = origin_domains
-  response['Access-Control-Allow-Origin'] = origin_domains
-
-  str = request.body.read
+  puts "params = #{params}"
+  str = JSON.parse(request.body.read)["expression"]
+  puts "expression = #{str}"
   result = Evaluator.new.evaluate(str, params || {})
+  puts "result = #{result}"
   result.to_s
 end
