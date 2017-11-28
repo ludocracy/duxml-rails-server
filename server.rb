5b30b9b65b14a26b5b217dad66f4a094ce74a01b
Peter Kong <bansuki@gmail.com>
2017-11-27 21:20:11 -0600

    updated reduxml version

--- server.rb
+++ server.rb
@@ -3,22 +3,25 @@ require 're_duxml'
 
 include ReDuxml
 
+# origin_domains = 'https://dry-draw.firebaseapp.com http://localhost:3000'
+origin_domains = '*'
+
 options '*' do
-    response.headers['Access-Control-Allow-Origin'] = 'https://dry-draw.firebaseapp.com'
-    response['Access-Control-Allow-Origin'] = 'https://dry-draw.firebaseapp.com'
+    response.headers['Access-Control-Allow-Origin'] = origin_domains
+    response['Access-Control-Allow-Origin'] = origin_domains
 end
 
 post '/resolveXML' do
-  response.headers['Access-Control-Allow-Origin'] = 'https://dry-draw.firebaseapp.com'
-  response['Access-Control-Allow-Origin'] = 'https://dry-draw.firebaseapp.com'
+  response.headers['Access-Control-Allow-Origin'] = origin_domains
+  response['Access-Control-Allow-Origin'] = origin_domains
 
   doc = Saxer.sax request.body.read
   resolve(doc, params).to_s
 end
 
 post '/evaluateStr' do
-  response.headers['Access-Control-Allow-Origin'] = 'https://dry-draw.firebaseapp.com'
-  response['Access-Control-Allow-Origin'] = 'https://dry-draw.firebaseapp.com'
+  response.headers['Access-Control-Allow-Origin'] = origin_domains
+  response['Access-Control-Allow-Origin'] = origin_domains
 
   str = request.body.read
   result = Evaluator.new.evaluate(str, params || {})
